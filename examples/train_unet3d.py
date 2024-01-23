8e26bd2912c4c80e0d83fe81cef46dd93b7d3b6d
mw <maximilian.weichart@icloud.com>
2024-01-23 12:28:46 +0100

    try expand

--- examples/train_unet3d.py
+++ examples/train_unet3d.py
@@ -94,6 +94,8 @@ if __name__ == "__main__":
         # Transforming?
         # requires_grad?
         image, label = Tensor(np.load(imgs_train[i]), requires_grad=False), Tensor(np.load(lbls_train[i]))
+        image = np.expand_dims(image, axis=0)
+        label = np.expand_dims(label, axis=0)
 
         out = model(image)
         loss = dice_ce_loss(out, label)
