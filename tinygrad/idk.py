2cd6866ffeeedca13b0cb872af7c3b6801629131
mw <maximilian.weichart@icloud.com>
2024-01-26 18:54:57 +0100

    conv2d

--- tinygrad/idk.py
+++ tinygrad/idk.py
@@ -1,7 +1,7 @@
 from tinygrad import Tensor, nn
 
 t = Tensor.rand((1, 32, 128, 128*128))
-conv = nn.Conv2d(32, 32, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
+conv = nn.Conv2d(32, 32, kernel_size=(3,3), padding=(1,1,1,1), bias=False)
 
 print("Realize conv 2d")
 conv_out = conv(t).realize()
