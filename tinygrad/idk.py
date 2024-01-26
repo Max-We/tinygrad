991069a787e0842e4e5aeecb8de3fd6b02b6f714
mw <maximilian.weichart@icloud.com>
2024-01-26 16:13:09 +0100

    realize x

--- tinygrad/idk.py
+++ tinygrad/idk.py
@@ -2,4 +2,7 @@ from tinygrad import Tensor, nn
 
 t = Tensor.rand(2, 256, 300, 300)
 conv = nn.Conv2d(256, 128, (3, 3), padding=1)
-print(conv(t).realize())
+
+print("Realize")
+wow = conv(t).realize()
+print("Done", wow.shape)
