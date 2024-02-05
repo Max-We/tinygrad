cc64c65dd4056be25380a315b273c6ffda986c0d
mw <maximilian.weichart@icloud.com>
2024-02-05 18:38:04 +0100

    comp eager

--- tinygrad/idk_torch.py
+++ tinygrad/idk_torch.py
@@ -10,6 +10,8 @@ input_tensor = torch.randn((1, channels, 128, 128, 128))
 # Define 3D convolution layer
 conv = nn.Conv3d(in_channels=channels, out_channels=channels, kernel_size=(3, 3, 3), padding=(1, 1, 1))
 
+torch.compiler.list_backends(exclude_tags=('debug', 'experimental'))
+
 # Apply convolution
 comp = torch.compile(conv, backend="eager")
 conv_out = comp(input_tensor)
