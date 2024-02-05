1bff19e838e3facea1f4177c518330ab9ceb5c33
mw <maximilian.weichart@icloud.com>
2024-02-05 18:14:53 +0100

    multi 3

--- tinygrad/idk.py
+++ tinygrad/idk.py
@@ -2,7 +2,10 @@ from tinygrad import Tensor, nn
 
 t = Tensor.rand((1, 32, 128, 128, 128))
 conv = nn.Conv2d(32, 32, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
-conv2 = nn.Conv2d(32, 32, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
+norm = nn.InstanceNorm(32)
+relu = Tensor.relu
+
+seq = [conv, norm, relu]
 
 # print("Realize conv")
 # conv_out = conv(t).realize()
@@ -12,11 +15,10 @@ conv2 = nn.Conv2d(32, 32, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False
 # mit viel ram & seq -> ?
 print("Realize multiple")
 
-a = conv(t)
-b = conv2(a)
-c = conv(b)
-d = conv2(c)
-print(d.realize().shape)
+a = t.sequential(seq)
+b = t.sequential(a)
+c = t.sequential(b)
+print(c.realize().shape)
 
 # print("Realize seq")
 # t.sequential([conv, conv, conv]).realize()
