23509f9a77592322a2b74079a42465ce71bc1a20
mw <maximilian.weichart@icloud.com>
2024-02-06 18:08:30 +0100

    optimize

--- tinygrad/idk.py
+++ tinygrad/idk.py
@@ -1,32 +1,23 @@
-from tinygrad import Tensor, nn
+from tinygrad import Tensor, nn, TinyJit, Device
 
+Device.DEFAULT="CPU"
 channels = 32
-t1 = Tensor.rand((1, channels, 128, 128, 128))
-t2 = Tensor.rand((1, channels, 128, 128, 128))
-conv1 = nn.Conv2d(channels, channels, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
-conv2 = nn.Conv2d(channels, channels, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
-norm = nn.InstanceNorm(channels)
-relu = Tensor.relu
 
-seq = [conv1, norm, relu]
+@TinyJit
+def do_ops():
+  with Tensor.train:
+    t = Tensor.rand((1, channels, 128, 128, 128))
+    conv = nn.Conv2d(channels, channels, kernel_size=(3,3,3), padding=(1,1,1,1,1,1), bias=False)
+    norm = nn.InstanceNorm(channels)
+    relu = Tensor.relu
 
-print("Realize conv 1")
-conv_out = conv1(t1).realize()
+    seq = [conv, norm, relu]
 
-print("Realize conv 2")
-conv_out = conv2(t2).realize()
+    print("Realize conv 1")
+    conv_out = conv(t).realize()
 
-# mit wenig ram -> fail
-# mit viel ram -> success
-# mit viel ram & seq -> ?
-# print("Realize multiple")
-#
-# a = t.sequential(seq)
-# b = t.sequential(a)
-# c = t.sequential(b)
-# print(c.realize().shape)
+    print(conv_out.shape)
 
-# print("Realize seq")
-# t.sequential([conv, conv, conv]).realize()
+do_ops()
 
 print("Done")
